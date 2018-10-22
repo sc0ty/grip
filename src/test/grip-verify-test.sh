fbdee02d8ad45c77ca0e0f454778676170dd9a69
Michal Szymaniak <m.szymaniak@adbglobal.com>
2018-10-22 16:22:51 +0200

    better grip verify sctipt

--- src/test/grip-verify-test.sh
+++ src/test/grip-verify-test.sh
@@ -4,19 +4,30 @@
 # CXX=afl-g++ VERIFY=1 make
 
 TESTCASES_DIR=".grip/testcases"
+OUTPUT_DIR="findings"
+DIFF_DIR="$OUTPUT_DIR/diffs"
 
-GRIP="../grip/grip -E -f"
-GREP="grep -rnIE --include=*.cpp -f"
+GRIP='../grip/grip -E -f'
+GREP='grep -rnIE --include="*.cpp" -f'
 
 find . -type f -name '*.cpp' | ../gripgen/gripgen
+
 mkdir -p "$TESTCASES_DIR"
-echo 'class' > "$TESTCASES_DIR/pattern1"
+echo 'class|int' > "$TESTCASES_DIR/pattern1"
 
-afl-fuzz -m 1000 -i "$TESTCASES_DIR" -o findings -- ../grip/grip-verify --abort --grip-cmd $GRIP @@ --ext-cmd $GREP @@
+eval "afl-fuzz -m 1000 -t 10 -i $TESTCASES_DIR -o $OUTPUT_DIR -- ../grip/grip-verify --abort --grip-cmd $GRIP @@ --ext-cmd $GREP @@"
 
-for f in findings/crashes/id*; do
-	$GRIP $f > "$f.grip"
-	$GREP $f > "$f.grep"
-	diff -u "$f.grep" "$f.grip" > "$f.diff"
+mkdir -p "$DIFF_DIR"
+
+shopt -s nullglob
+for f in $OUTPUT_DIR/crashes/*; do
+	name=`basename "$f"`
+
+	if [ "$name" != "README.txt" ]; then
+		echo " - $name"
+		eval "$GRIP $f" > "$DIFF_DIR/$name.grip"
+		eval "$GREP $f" > "$DIFF_DIR/$name.grep"
+		diff -u "$DIFF_DIR/$name.grep" "$DIFF_DIR/$name.grip" > "$DIFF_DIR/$name.diff"
+	fi
 done
 
